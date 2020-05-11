298bb7f825dc011e1528c6fbd5b40d647923e21f
standmarsh <agrawalshagun07@gmail.com>
2020-05-12 00:11:12 +0530

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,50 @@
-import React, { Fragment, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Landing from "./components/layout/Landing";
-import Alert from "./components/layout/Alert";
-import "./App.css";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
+import './App.css';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token);
 }
 
 const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  )
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<section className="container">
+						<Alert />
+						<Switch>
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
+							<PrivateRoute exact path="/create-profile" component={CreateProfile} />
+							<PrivateRoute exact path="/edit-profile" component={EditProfile} />
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
 };
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,55 +1,73 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
+	const [ formData, setFormData ] = useState({
+		email: '',
+		password: ''
+	});
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    });
+	const { email, password } = formData;
 
-    const { email, password } = formData;
+	const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		login(email, password);
+	};
 
-    const onSubmit = async e => {
-        e.preventDefault();
-        console.log('Success');
-    }
+	// redirect if logged in
+	if (isAuthenticated) {
+		return <Redirect to="/dashboard" />;
+	}
 
-    return (
-        <Fragment>
-            <h1 className="large text-primary">Sign In</h1>
-            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
-            <form className="form" onSubmit={e => onSubmit(e)}>
+	return (
+		<Fragment>
+			<h1 className="large text-primary">Sign In</h1>
+			<p className="lead">
+				<i className="fas fa-user" /> Sign Into Your Account
+			</p>
+			<form className="form" onSubmit={(e) => onSubmit(e)}>
+				<div className="form-group">
+					<input
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						minLength="6"
+						value={password}
+						onChange={(e) => onChange(e)}
+					/>
+				</div>
 
-                <div className="form-group">
-                    <input type="email"
-                        placeholder="Email Address"
-                        name="email"
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required />
+				<input type="submit" className="btn btn-primary" value="Login" />
+			</form>
+			<p className="my-1">
+				Don't have an account? <Link to="/register">Sign Up</Link>
+			</p>
+		</Fragment>
+	);
+};
 
-                </div>
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        minLength="6"
-                        value={password}
-                        onChange={e => onChange(e)}
-                    />
-                </div>
+Login.propTypes = {
+	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool
+};
 
-                <input type="submit" className="btn btn-primary" value="Login" />
-            </form>
-            <p className="my-1">
-                Don't have an account? <Link to="/register">Sign Up</Link>
-            </p>
-        </Fragment>
-    )
-}
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
 
-export default Login;
+export default connect(mapStateToProps, { login })(Login);
