48ac61d274a539be174ab56862acc29e3d4f9d63
standmarsh <agrawalshagun07@gmail.com>
2020-05-22 17:52:39 +0530

    adding reamining changes

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,25 +3,32 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({
+	deletePost,
+	addLike,
+	removeLike,
+	auth,
+	post: { _id, text, name, avatar, user, likes, comments, date }
+}) => {
 	return (
 		<div class="post bg-white p-1 my-1">
 			<div>
-				<a href="profile.html">
+				<Link to={`/profile/${user}`}>
 					<img class="round-img" src={avatar} alt="" />
 					<h4>{name}</h4>
-				</a>
+				</Link>
 			</div>
 			<div>
 				<p class="my-1">{text}</p>
 				<p class="post-date">
 					Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
 				</p>
-				<button type="button" class="btn btn-light">
+				<button onClick={(e) => addLike(_id)} type="button" class="btn btn-light">
 					<i class="fas fa-thumbs-up" /> <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
 				</button>
-				<button type="button" class="btn btn-light">
+				<button onClick={(e) => removeLike(_id)} type="button" class="btn btn-light">
 					<i class="fas fa-thumbs-down" />
 				</button>
 				<Link to={`/post/${_id}`} class="btn btn-primary">
@@ -30,7 +37,7 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
 				</Link>
 				{!auth.loading &&
 				user === auth.user._id && (
-					<button type="button" class="btn btn-danger">
+					<button onClick={(e) => deletePost(_id)} type="button" class="btn btn-danger">
 						<i class="fas fa-times" />
 					</button>
 				)}
@@ -41,11 +48,14 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
 
 PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
-	auth: PropTypes.object.isRequired
+	auth: PropTypes.object.isRequired,
+	addLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem);
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
-import PostItem from "./PostItem";
+import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
 	useEffect(
@@ -21,7 +22,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
 			<p className="lead">
 				<i className="fas fa-user" /> Welcome to the community
 			</p>
-			{/* PostForm */}
+			<PostForm />
 			<div className="post">{posts.map((post) => <PostItem key={post._id} post={post} />)}</div>
 		</Fragment>
 	);
