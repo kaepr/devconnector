e360b81db6c101e90235699318efe0f8ca8f7874
standmarsh <agrawalshagun07@gmail.com>
2020-05-22 22:46:57 +0530

    final changes

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,13 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from '../actions/types';
+import {
+	GET_POSTS,
+	POST_ERROR,
+	UPDATE_LIKES,
+	DELETE_POST,
+	ADD_POST,
+	GET_POST,
+	ADD_COMMENT,
+	REMOVE_COMMENT
+} from '../actions/types';
 
 const initialState = {
 	posts: [],
@@ -15,6 +24,12 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				posts: payload,
+				loading: false
+			};
+		case GET_POST:
+			return {
+				...state,
+				post: payload,
 				loading: false
 			};
 		case ADD_POST:
@@ -39,6 +54,21 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				posts: state.posts.filter((post) => post._id !== payload),
+				loading: false
+			};
+		case ADD_COMMENT:
+			return {
+				...state,
+				post: { ...state.post, comments: payload },
+				loading: false
+			};
+		case REMOVE_COMMENT:
+			return {
+				...state,
+				post: {
+					...state.post,
+					comments: state.post.comments.filter((comment) => comment._id !== payload)
+				},
 				loading: false
 			};
 		default:
